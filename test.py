c7e109dfe39e5a3eadc034537b3d7a31677a5b0b
Karan Singh <iamkaransingh@gmail.com>
2020-01-17 18:50:06 -0500

    wow3

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ import numpy as np
 
 env = PlanarQuadrotor()
 for t in np.linspace(0., 1., num=100, endpoint=True):
-    env.render(np.array([t, t**2, 0., 0., 0., 0.]))
-    time.sleep(0.1)
+    env.render(np.array([t, t**2, np.pi*t*1., 0., 0., 0.]))
+    time.sleep(0.01)
