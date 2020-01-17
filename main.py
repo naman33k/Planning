e48a069ebbe406de463478218f6d5d142b4faf82
Naman <naman33k@gmail.com>
2020-01-17 18:57:07 -0500

    Quadrotor ILQR

--- main.py
+++ main.py
@@ -27,13 +27,14 @@ u0 = np.tile(quadrotor.hover_input, (quadrotor.h, 1)) + 0.0*jax.random.uniform(k
 print(quadrotor.initial_state)
 print(quadrotor.step(quadrotor.initial_state, quadrotor.hover_input, 1, is_real_dynamics=False))
 
-final_actions, costs, states = IPA(quadrotor, u0, 10)
-# print(costs)
+final_actions, costs, states = IPA(quadrotor, u0, 3)
+print(costs)
+print(states[-1])
 # print("Rendering the found action now")
 # for i in range(pendulum.h+1):
 # 	if i==0:
 # 		pendulum.render(states[i])
-# 		time.sleep(1. / 30.)
+# 		time.sleep(1. / 20.)
 # 	else:
 # 		pendulum.render(states[i], last_u=final_actions[i-1])
-# 		time.sleep(1. / 30.)
+# 		time.sleep(1. / 20.)
